7cabd63cfbd00162b620a7a249fbd1ce495341a1
Nathan Bedford <nathan.bedford@gmail.com>
2025-12-17 09:20:52 -0500

    Removed tanstack router on prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,13 @@ import { createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import App from '../App';
 
+const isDev = import.meta.env.MODE === 'development';
+
 export const Route = createRootRoute({
   component: () => (
     <>
       <App />
-      <TanStackRouterDevtools />
+      {isDev && <TanStackRouterDevtools />}
     </>
   ),
 });
